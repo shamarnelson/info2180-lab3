fbbd1636433745b88aac5a57da82f2666e121d53
shamarnelson <50060085+shamarnelson@users.noreply.github.com>
2020-10-11 03:11:53 -0500

    Edited JavaScript

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -3,15 +3,22 @@
 window.onload=()=>{
     let n = document.querySelectorAll("div#board div");
     let isX= true;
+    let button=document.querySelector(".btn");
+    let status=document.getElementById("status");
+    let statusIntialText=status.innerHTML;
+    let g=Array(9).fill(0);
+    n.forEach((i,r)={
+        i.classList.add("square");
+    
 
     //ex2
-    i.addEventListener("userclick",()=>{
-        if( game[n]=== 0 ){
+    i.addEventListener("click",()=>{
+        if( game[r]=== 0 ){
             if(isX === true){
                 i.classList.add("X")
                 i.innerHTML= "X"
                 isX=false;
-                game[n]="X";
+                game[r]="X";
 
 
             }else{
@@ -19,11 +26,11 @@ window.onload=()=>{
                 i.innerHTML="O"
                 isX=true;
 
-                game[n]="O";
+                game[r]="O";
             }
         }
 
-        //Ex3
+        //ex3
         i.addEventListener("mouover",()=>{
             i.classList.add("hover")
         })
@@ -33,7 +40,7 @@ window.onload=()=>{
         })
 
 
-        /*  console.log(game);
+         
 
         let top=[game[0],game[1],game[2]];
         let mid=[game[3],game[4],game[5]];
@@ -43,12 +50,53 @@ window.onload=()=>{
         let midVert=[game[1],game[4],game[7]]
         let lowVert=[game[2],game[5],game[8]]
 
-        let diag1=[game[0],game[4],game[8]]
-        let diag2=[game[2],game[4],game[6]]
-        //if([])
+        let diagA=[game[0],game[4],game[8]]
+        let diagB=[game[2],game[4],game[6]]
+        
+        //ex4
+        let result= ""
+        let oVert= topVert.every((x)=> x === "O") || midVert.every((x)=> x === "O") || lowVert.every((x)=>x === "O")
+        let xVert= topVert.every((x)=> x === "X") || midVert.every((x)=> x === "X") || lowVert.every((x)=>x === "X")
 
-        let 
-    }
+        let xHor= top.every((x)=> x === "X") || mid.every((x)=> x === "X") || low.every((x)=>x === "X") 
+        let oHor= top.every((x)=> x === "O") || mid.every((x)=> x === "O") || low.every((x)=>x === "O") 
 
+        let xDiag= diagA.every((x)=> x=== "X" || diagB.every((x)=>x==="X")
+        let oDiag= diagA.every((x)=> x=== "O" || diagB.every((x)=>x==="O")
+
+        if (xVert){
+            result="Congratulations! X is the Winner!"
+        }
+        else if(oVert){
+            result="Congratulations! O is the Winner!"
+        }
+        else if(xHor){
+            result="Congratulations! X is the Winner!"
+        }
+        else if(oHor){
+            result="Congratulations! O is the Winner!"
+        }
+        else if(xDiag){
+            result="Congratulations! X is the Winner!"
+        }  
+        else if(oDiag){
+            result="Congratulations! O is the Winner!"
+        } 
+        
+        if(result!=""){
+            status.innerHTML=result;
+        }
+
+
+    })
+
+    button.addEventListener("click",()=>{
+        a.forEach((e) => {
+            e.innerHTML=""
+            g=Array(9).fill(0);
+            status.innerHTML=statusIntialText;
+            
+        });
+    })
 }
-*/
+
